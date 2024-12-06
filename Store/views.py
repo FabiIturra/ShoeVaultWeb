014a1db8db3681f6b0d44323accf63ca925b4693
Fabian_Iturra <fab.iturra@duocuc.cl>
2024-12-05 21:56:36 -0300

    inicio proyecto web

---
+++ Store/views.py
@@ -0,0 +1,32 @@
+from io import BytesIO
+from django.http import HttpResponse
+from django.shortcuts import render
+from django_user_agents.utils import get_user_agent
+import qrcode
+
+# Create your views here.
+
+
+def index_view(request):
+    user_agent = get_user_agent(request)
+
+    is_movile = user_agent.is_mobile or user_agent.is_tablet
+    is_notebook = not is_movile
+
+    context = {
+        'is_notebook': is_notebook,
+        'is_phone': is_movile,
+    }
+
+    return render(request, 'index.html', context)
+
+def generate_qr(request):
+    qr_url = 'https://drive.usercontent.google.com/download?id=1OOt8fg5IC1aahvW2Q4GQArv4Na4xdN5l&export=download&authuser=0'
+
+    qr = qrcode.make(qr_url)
+
+    qr_image = BytesIO()
+    qr.save(qr_image)
+    qr_image.seek(0)
+
+    return HttpResponse(qr_image, content_type="image/png")
